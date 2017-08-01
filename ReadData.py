a2923e9c8c3e909e0873019fa96ed74a45fcf904
unknown <>
2017-08-01 23:10:02 +0800

    A script for processing MRT log

--- ReadData.py
+++ ReadData.py
@@ -1,25 +1,25 @@
 import pandas as pd
 import numpy as np
+import os
 
-x = np.arange(0, 5, 1)
-y = np.array([[0.37,0.14,0.75,0.70,0.56],[0.02,0.37,0.78,0.12,
-			0.17],[0.62,0.72,0.15,0.33,0.20],[0.39,0.37,0.01,
-			0.12,0.53]])
-			
-print 			
+def eachFile(filepath):
+	PathList = []
+	pathDir = os.listdir(filepath)
+	for allDir in pathDir:
+		child = os.path.join('%s\\%s'%(filepath,allDir))
+		print pd.read_table(child, skiprows = 14, header = 0)
+		#PathList.append(child)
+		#print child.decode('gbk')
+	#print PathList
 
-plt.figure(figsize=(8,4))
-for i in range(4):
-	plt.plot(x, y[i,:], label="Temperature of Antenna ID %d" %i, color="red", linewidth=2)
-	#plt.plot(x,z, "b--", label="$cos(x^2)$")
+filepath = raw_input("Please enter FilePath: ==>")
 
-plt.xlabel("Time(s)")
-plt.ylabel("Degree C")
+eachFile(filepath)
 
-plt.title("Temperature Data of Antenna")
-plt.ylim(0, 1)
-#plt.xlim(0,100)
 
-plt.legend()
+#pd.read_table("D:\dataprocess\MRT_log\EB2-SH-003&004_03,15,17_02,27,50PM", skiprows = 14,header = 0)
 
-plt.show()
+#print df
+
+#print df.columns
+
